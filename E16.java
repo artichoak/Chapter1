865e887802758754a2da217dcc7a5ca8610ed364
artichoak <apurva@oakorinda.com>
2016-09-03 10:47:03 -0700

    Add files via upload

---
+++ E16.java
@@ -0,0 +1,19 @@
+
+/**
+ * A P U R V A
+ * 
+ * @author Apurva 
+ * @version 6/21/16
+ */
+public class E16
+{
+   public static void main (String[] args)
+   {
+       System.out.println("  *   ***  *  * ***  *   *   *  ");
+       System.out.println(" * *  *  * *  * *  * *   *  * * ");
+       System.out.println("*   * ***  *  * ***   * *  *   *");
+       System.out.println("***** *    *  * * *   * *  *****");
+       System.out.println("*   * *    **** *  *   *   *   *");
+      
+   }
+}
