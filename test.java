7b9d805991c642a866968de76c4c76472725f09f
artichoak <apurva@oakorinda.com>
2016-09-03 10:36:10 -0700

    Update test.java

--- test.java
+++ test.java
@@ -1,9 +1,9 @@
 
 /**
- * Write a description of class Proj1 here.
+ * Project 1.1 
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Apurva Oak
+ * 1.0
  */
 public class test
 {
@@ -12,3 +12,6 @@ public class test
         System.out.println ("An emergency Broadcast");
     }
 } 
+// when I chanced Test to test, the class name changes
+// when I changed Emergency to emergency the text changed 
+// when I removed the quotation mark, there was an error
